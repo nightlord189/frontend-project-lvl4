2bae376bfccaa5cd27a37fdc4b3c12cee94496de
Alexey Buravov <night.lord.189@gmail.com>
2021-06-13 05:08:45 +0600

    logout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,27 +9,35 @@ import Login from './Login.jsx';
 import NotFound from './NotFound.jsx';
 import Chat from './Chat/Chat.jsx';
 
-const App = () => (
-  <Router>
-    <div className="d-flex flex-column h-100">
-      <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-        <div className="container">
-          <Link to="/" className="navbar-brand">Hexlet Chat</Link>
-        </div>
-      </nav>
-      <Switch>
-        <Route exact path="/">
-          <Chat />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-);
+const App = () => {
+  const logout = () => {
+    localStorage.removeItem('userToken');
+    window.location.href = '/login';
+  };
+
+  return (
+    <Router>
+      <div className="d-flex flex-column h-100">
+        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+          <div className="container">
+            <Link to="/" className="navbar-brand">Hexlet Chat</Link>
+            <button onClick={logout} type="button" className="btn btn-primary">Выйти</button>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Chat />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
