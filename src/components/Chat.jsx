01a71a3a37060f9407f1ef3417ce5163a7e84ed8
Alexey Buravov <night.lord.189@gmail.com>
2021-06-13 02:06:27 +0600

    download data on Chat page

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 const Chat = () => {
   const authToken = localStorage.getItem('userToken');
   if (!authToken) {
     window.location.href = '/login';
   }
+
+  const getData = async () => {
+    const response = await axios.get('/api/v1/data',
+      {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  });
+
   return (
     <div><h1>Chat</h1></div>
   );
