ecbc9d955e865bdc4900548128641c9fd8919cd2
Alexey Buravov <night.lord.189@gmail.com>
2021-06-20 13:16:04 +0600

    work on tests

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ import addRoutes from './routes.js';
 const { Unauthorized } = HttpErrors;
 
 // eslint-disable-next-line no-underscore-dangle
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url); // eslint-disable-line
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(__filename);
 
